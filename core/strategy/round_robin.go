4b49e14bf58cc7647805ae1ec759639dfedc3238
Sahil-796 <sp0709006@gmail.com>
2025-11-16 15:43:54 +0530

    config: threshold inclusion fix: filename

---
+++ core/strategy/round_robin.go
@@ -0,0 +1,42 @@
+package strategy
+
+import (
+	"github.com/Sahil-796/golem/types"
+	"sync"
+)
+
+type RoundRobin struct {
+	index int
+	Mutex sync.Mutex
+} 
+
+func (rr *RoundRobin) NewRoundRobin () *RoundRobin {
+	return new(RoundRobin)
+}
+
+func (rr *RoundRobin) Next(servers []types.Server) *types.Server {
+	
+	if len(servers) == 0 {
+		return nil
+	} 
+	
+	rr.Mutex.Lock()
+	defer rr.Mutex.Unlock()
+	n:=len(servers)
+	
+	for range(n) {
+		
+		s := &servers[rr.index]
+		rr.index = (rr.index +1) % n
+		
+		s.Mutex.Lock()
+		healthy := s.IsHealthy
+		s.Mutex.Unlock()
+		
+		if healthy { 
+			return s
+		}
+	}
+	
+	return nil
+}
